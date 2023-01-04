c1f7e91bc8e624fb2d9be451599f47e9fa475478
Anuragd275 <tonythestark7@gmail.com>
2023-01-04 10:12:24 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    colors:{
+      "cyan": "#70F1D5",
+      "cyan-light": "#CAFFF3"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
